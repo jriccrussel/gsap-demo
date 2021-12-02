c3ebc588b2fa41be9d2decab2e27226a001a4c58
jriccrussel <jriccrussel@gmail.com>
2021-12-03 05:06:46 +0800

    navigation links

--- 00-Full-Page/js/main.js
+++ 00-Full-Page/js/main.js
@@ -1,11 +1,27 @@
 gsap.registerPlugin(ScrollTrigger);
 
-function init(){
-    
-    // start here
+function initNavigation(){
 
+    const mainNavLinks = gsap.utils.toArray('.main-nav a');
+
+    // Hover class animate
+    mainNavLinks.forEach(link => {
+        link.addEventListener('mouseleave', e => {
+            link.classList.add('animate-out'); // when ang link(navigation link) is hovered then run class animate
+
+            setTimeout(() => {
+                link.classList.remove('animate-out') // if dili cya hovered(navigation link) then remove class animate
+            }, 300)
+        })
+    })
+    
+}
+
+function init(){    
+    // Calling the function to run
+    initNavigation();
 }
 
 window.addEventListener('load', function(){
-    init();
+    init(); 
 });
