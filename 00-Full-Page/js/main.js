1a16150067c94ca387213c3d1de8add09a0ac55d
jriccrussel <jriccrussel@gmail.com>
2021-12-04 10:31:08 +0800

    changes

--- 00-Full-Page/js/main.js
+++ 00-Full-Page/js/main.js
@@ -64,7 +64,7 @@ function moveImages(e){
 
     // get 0 0 in the center
     const xPos = offsetX / clientWidth - 0.5
-  const yPos = offsetY / clientHeight - 0.5
+    const yPos = offsetY / clientHeight - 0.5
 
     const leftImages = gsap.utils.toArray('.hg__left .hg__image');
     const rightImages = gsap.utils.toArray('.hg__right .hg__image');
