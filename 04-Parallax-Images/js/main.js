6e07f163bf9c6359988f0141cdb48309e63d7f8d
jriccrussel <jriccrussel@gmail.com>
2021-12-06 12:41:42 +0800

    parallax setup scrollTrigger

--- 04-Parallax-Images/js/main.js
+++ 04-Parallax-Images/js/main.js
@@ -1,5 +1,29 @@
 gsap.registerPlugin(ScrollTrigger);
 
+function initImageParallax(){
+    
+    // select all sections .with parallax
+    gsap.utils.toArray('.with-parallax').forEach(section => {
+
+        // get time image
+        const image = section.querySelector('img');
+
+        // create tween for time iamge
+        gsap.to(image, {
+            yPercent: 20,
+            ease: 'none',
+            scrollTrigger: {
+                trigger: section,
+                start: 'top bottom',
+                scrub: true,
+                markers: true
+            }
+        })
+
+    })
+
+}
+
 function init(){
     
     // start here
