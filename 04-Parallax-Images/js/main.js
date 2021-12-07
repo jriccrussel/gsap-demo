8f9ec9d5ae9738f69a400c5f81dfad05fc90d07a
jriccrussel <jriccrussel@gmail.com>
2021-12-07 23:13:26 +0800

    bug fix from initPinSteps || needs to scroll up && down slowly for the pin nav to work

--- 04-Parallax-Images/js/main.js
+++ 04-Parallax-Images/js/main.js
@@ -16,8 +16,8 @@ function initImageParallax(){
             scrollTrigger: {
                 trigger: section,
                 start: 'top bottom',
-                scrub: true,
-                // scrub: 1,
+                // scrub: true,
+                scrub: 1,
                 // markers: true
             }
         });
@@ -25,7 +25,17 @@ function initImageParallax(){
 }
 
 // PIN NAVIGATION
+// Pin Navigation works but when Gsap Smooth Scroll was added thus it needs to scroll up & down slowly kai nag add ta ug 'pinReparent' sa scrolltrigger(fixed nav) it changes the position from section 'how-we-work' to next sa 'body'(up and down scroll)
 function initPinSteps(){
+
+    // ScrollTrigger.create({
+    //     trigger: '.fixed-nav',
+    //     start: 'top center',
+    //     endTrigger: '#stage4',
+    //     end: 'center center',
+    //     pin: true,
+    //     pinReparent: true
+    // });
 
     ScrollTrigger.create({
         trigger: '.fixed-nav',
@@ -34,7 +44,7 @@ function initPinSteps(){
         end: 'center center',
         pin: true,
         // markers: true,
-        // pinReparent: true
+        pinReparent: true
     });
 
     // get true hieght even sa mobile
@@ -92,37 +102,37 @@ function init(){
     initScrollTo();
 }
 
-window.addEventListener('load', function(){
-    init();
-});
-
-// // GSAP SMOOTH SCROLLING
 // window.addEventListener('load', function(){
 //     init();
 // });
 
-// let container = document.querySelector('#scroll-container');
+// // GSAP SMOOTH SCROLLING
+window.addEventListener('load', function(){
+    init();
+});
+
+let container = document.querySelector('#scroll-container');
 // // document.body.style.height = container.clientHeight + "px";
 // // document.body.style.height = `${container.clientHeight}px`;
 
-// let height;
-// function setHeight(){
-//     height = container.clientHeight;
-//     document.body.style.height = `${height}px`;
-// }
-// ScrollTrigger.addEventListener('refreshInit', setHeight);
+let height;
+function setHeight(){
+    height = container.clientHeight;
+    document.body.style.height = `${height}px`;
+}
+ScrollTrigger.addEventListener('refreshInit', setHeight);
 
-// gsap.to(container, {
-//     y: () => -(height - document.documentElement.clientHeight),
-//     // ease: 'Power2.out',
-//     ease: 'none',
-//     scrollTrigger: {
-//         trigger: document.body,
-//         start: 'top top',
-//         end: 'bottom bottom',
-//         // scrub: 3,
-//         scrub: 1,
-//         invalidateOnRefresh: true,
-//         markers: true
-//     }
-// })
+gsap.to(container, {
+    y: () => -(height - document.documentElement.clientHeight),
+    // ease: 'Power2.out',
+    ease: 'none',
+    scrollTrigger: {
+        trigger: document.body,
+        start: 'top top',
+        end: 'bottom bottom',
+        // scrub: 3,
+        scrub: 1,
+        invalidateOnRefresh: true,
+        // markers: true
+    }
+})
