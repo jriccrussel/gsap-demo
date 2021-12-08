c3842d510991b992454b106e70c62d3a0b27a061
jriccrussel <jriccrussel@gmail.com>
2021-12-08 15:49:43 +0800

    loading animation in && out

--- 05-Intro-Animation/js/main.js
+++ 05-Intro-Animation/js/main.js
@@ -1,9 +1,68 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const select = (e) => document.querySelector(e);
+const selectAll = (e) => document.querySelectorAll(e);
+
+// Loader
+function initLoader(){
+
+    // Loader Animate In
+    const tlLoaderIn = gsap.timeline({
+        id: 'tlLoaderIn',
+        defaults: {
+            duration: 1.1,
+            ease: 'power2.out'
+        },
+        onComplete: () => select('body').classList.remove('is-loading')
+    });
+
+    const loaderInner = select('.loader .inner');
+    const image = select('.loader__image img');
+    const mask = select('.loader__image--mask');
+    const line1 = select('.loader__title--mask:nth-child(1) span'); // TItle 1
+    const line2 = select('.loader__title--mask:nth-child(2) span'); // TItle 2
+    const lines = selectAll('.loader__title--mask');
+    const loader = select('.loader');
+    const loaderContent = select('.loader__content');
+
+    tlLoaderIn
+    .from(loaderInner, {
+        scaleY: 0,
+        transformOrigin: 'bottom'
+    }).addLabel('revealImage')
+    .from(mask, {yPercent: 100}, 'revealImage-=0.6')  
+    .from(image, {yPercent: 100}, 'revealImage-=0.6')
+    .from([line1, line2], {yPercent: 100, stagger: 0.1}, 'revealImage-=0.4');
+
+    // Loader Animate Out
+    const tlLoaderOut = gsap.timeline({
+        id: 'tlLoaderOut',
+        defaults: {
+            duration: 1.2,
+            ease: 'power2.inOut'
+        },
+        delay: 1
+    })
+
+    tlLoaderOut
+    .to(lines, {yPercent: -500, stagger: 0.2}, 0)
+    .to([loader, loaderContent], {yPercent: -100}, 0.2)
+    .from('#main', {y: 150}, 0.2);
+
+    const tlLoader = gsap.timeline();
+
+    tlLoader
+    .add(tlLoaderIn)
+    .add(tlLoaderOut)
+
+    GSDevTools.create({paused: true});
+
+}
+
 function init(){
     
     // start here
-
+    initLoader();
 }
 
 window.addEventListener('load', function(){
