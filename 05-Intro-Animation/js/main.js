67eba76781ed1621df078d67d4d1fa745e335f6f
jriccrussel <jriccrussel@gmail.com>
2021-12-08 15:54:49 +0800

    optimize loader

--- 05-Intro-Animation/js/main.js
+++ 05-Intro-Animation/js/main.js
@@ -26,6 +26,7 @@ function initLoader(){
     const loaderContent = select('.loader__content');
 
     tlLoaderIn
+    .set([loader, loaderContent], {autoAlpha: 1})
     .from(loaderInner, {
         scaleY: 0,
         transformOrigin: 'bottom'
